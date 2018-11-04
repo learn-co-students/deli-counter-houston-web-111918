5cfac2278ede799b5c414b970b77d2dd51942f86
github username <github email address>
2018-11-04 14:53:21 -0600

    deli counter

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def line(katz_deli)
+    frag = []
+  if katz_deli == []
+      puts "The line is currently empty."
+    else
+      katz_deli.each_with_index do |order, counter|
+        frag << ("#{counter += 1}. #{order}")
+
+    end
+    puts "The line is currently: #{frag.join(" ")}"
+
+  end
+end
+
+def take_a_number(array, name)
+
+  if
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+end
+
+def now_serving(katz_deli)
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
